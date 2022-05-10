a173799932fb0fc9e27860d5ab1157cd54ef15f5
Sidiki Sako <sidiksako@gmail.com>
2022-05-10 16:30:52 +0200

    initilisation de flame game

---
+++ lib/flappy_game.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class FlappyGame with Game {
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+  }
+}
